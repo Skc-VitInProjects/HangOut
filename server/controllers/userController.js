55212c7717aaae60468d8e5dde73590e67237db3
Skand-VitInProjects <iskc9838@gmail.com>
2025-12-16 21:35:50 +0530

    Starting with image

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,3 +17,38 @@ export const getUserData = async(req, res) => {
           res.json({success: false , message: error.message});
      }
 }
+
+// Update User Data
+export const updateUserData = async(req, res) => {
+     try{
+          const {userId} = req.auth();
+          const {username, bio , location, full_name} = req.body;
+
+          const tempUser = await User.findById(userId);
+
+          !username && (username = tempUser.username)
+
+          if(tempUser.username !== username){
+             const user = User.findOne({username});
+
+             if(user){
+               //we will not change the username if it is already taken
+               username = tempUser.username;
+             }
+          }
+
+          const updatedData = {
+               username,
+               bio,
+               location,
+               full_name
+          }
+
+          const profile = req.files.profile && req.files.profile[0];
+          const cover = req.files.cover && req.files.cover[0];
+
+     }catch(error){
+          console.log(error);
+          res.json({success: false , message: error.message});
+     }
+}
