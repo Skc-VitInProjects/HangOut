4a8fd8fdfa3fb16213873b483491076418229a46
Skand-VitInProjects <iskc9838@gmail.com>
2025-12-23 22:42:05 +0530

    Done with add Post function

---
+++ server/controllers/postController.js
@@ -0,0 +1,54 @@
+import imagekit from "../configs/imageKit.js";
+import fs from 'fs';
+import Post from "../models/Post.js";
+
+// Add Post
+export const addPost = async (req, res) => {
+     try {
+          const { userId } = req.auth();
+          const { content, post_type } = req.body;
+          const images = req.files
+
+          let image_urls = []
+
+          if (images.length) {
+               image_urls = await Promise.all(
+                    images.map(async (image) => {
+                         const fileBuffer = fs.readFileSync(image.path)
+
+                         const response = await imagekit.upload({
+                              file: fileBuffer,
+                              filename: image.originalname,
+                              folder: "posts",
+                         })
+
+                         const url = imagekit.url({
+                              path: response.filePath,
+                              transformation: [
+                                   { quality: 'auto' },
+                                   { format: 'webp' },
+                                   { width: '1280' },
+
+                              ]
+                         })
+
+                         return url
+                    })
+               )
+          }
+
+        await Post.create({
+           user: userId,
+           content,
+           image_urls,
+           post_type
+        })
+
+        res.json({success: true, message: "Post created successfully"});
+
+     } catch (error) {
+          console.log(error);
+          res.json({success: false, message: error.message});
+          
+     }
+}
