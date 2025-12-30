7d519829dcdffa1646cb4aeee61ed3427caee942
Skand-VitInProjects <iskc9838@gmail.com>
2025-12-30 22:32:21 +0530

    Done with notification, recent messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Login from './pages/Login'
 import Feed from './pages/Feed'
 import Messages from './pages/Messages'
@@ -10,15 +10,19 @@ import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import Layout from './pages/Layout'
 import {useUser, useAuth} from '@clerk/clerk-react'
-import {Toaster} from 'react-hot-toast'
-import { useEffect } from 'react'
+import toast, {Toaster} from 'react-hot-toast'
+import { useEffect, useRef } from 'react'
 import { useDispatch} from 'react-redux'
 import {fetchUser} from './features/user/userSlice.js'
 import { fetchConnections } from './features/connections/connectionsSlice.js'
+import { addMessage } from './features/messages/messagesSlice.js'
+import Notification from './components/Notification.jsx'
 
 const App = () => {
   const {user} = useUser()
   const {getToken} = useAuth()
+  const {pathname} = useLocation()
+  const pathnameRef = useRef(pathname)
 
   const dispatch = useDispatch()
 
@@ -34,6 +38,32 @@ const App = () => {
   fetchData()
   }, [user, getToken , dispatch])
 
+  useEffect(()=> {
+    pathnameRef.current = pathname
+
+  }, [pathname])
+
+  useEffect(()=> {
+    if(user){
+      const eventSource = new EventSource(import.meta.env.VITE_BASEURL + '/api/message/' + user.id);
+
+      eventSource.onmessage = (event)=> {
+           const message = JSON.parse(event.data)
+
+           if(pathnameRef.current === ('/messages/' + message.from_user_id._id )){
+             dispatch(addMessage(message))
+           }else{
+             toast.custom((t)=> (
+              <Notification t={t} message={message}/>
+             ), {position: "bottom-right"})
+           }
+      }
+
+      return ()=> {
+        eventSource.close()
+      }
+    }
+  },[user, dispatch])
   return (
     <> 
 
